8c0d1dbb6b3975a65665533de62daaea65959164
lee-song-yi <okpo5071@gmail.com>
2023-10-18 10:31:08 +0900

    페이지별 타이틀 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import MainPage from "../views/MainView.vue";
 import SearchResult from "../views/SearchView.vue";
+import testView from "../views/testView.vue";
 import TempDetail from "../views/modal/TempDetail.vue";
 // import PubList from "../views/PubList.vue";
 
@@ -9,16 +10,27 @@ const routes = [
     path: "/",
     name: "MainView",
     component: MainPage,
+    meta: {
+      title : 'OKMS | 메인'
+    }
   },
   {
     path: "/search",
     name: "SearchResult",
     component: SearchResult,
+    meta: {
+      title : 'OKMS | 통합검색'
+    }
   },
   {
     path: "/modal/template_detail",
     name: "TempDetail",
     component: TempDetail,
+  },
+  {
+    path: "/test",
+    name: "testView",
+    component: testView,
   },
   // {
   //   path: "/pub_list",
@@ -32,4 +44,8 @@ const router = createRouter({
   routes,
 });
 
+router.afterEach((to) => {
+  const title = to.meta.title || 'OKMS';
+  document.title = title;
+});
 export default router;
